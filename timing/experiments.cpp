e39391be40664f98343eb44030d5be9a9bf49a5f
Tamara Mitrovska <tamaram@mit.edu>
2022-04-13 12:25:33 -0400

    added timing configs with padding

--- timing/experiments.cpp
+++ timing/experiments.cpp
@@ -103,22 +103,22 @@ void time_specialized_conv2d(int iw, int ih, int ww, int wh, int b_sz, int in_ch
 }
 
 void run() {
-    int n_runs = 6;
-    int iw[] = {100, 100, 200, 200, 300, 300};
-    int ih[] = {100, 100, 200, 200, 300, 300};
-    int kw[] = {10, 10, 10, 10, 10, 10};
-    int kh[] = {10, 10, 10, 10, 10, 10};
-    int batch_size[] = {10, 20, 10, 20, 10, 20};
-    int in_channels[] = {10, 10, 10, 10, 10, 10};
-    int out_channels[] = {10, 10, 10, 10, 10, 10};
-    int stride[][2] = {{1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}};
-    int padding[][2] = {{0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}};
-    int dilation[][2] = {{1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}};
-    int padding_same[] = {0, 0, 0, 0, 0, 0, 0, 0};
-    std::string func_names[] = {"f1", "f2", "f3", "f4", "f5", "f6"};
-    GeneratedFunction functions[] = {&f1, &f2, &f3, &f4, &f5, &f6};
+    int n_runs = 9;
+    int iw[] = {100, 100, 200, 200, 300, 300, 200, 200, 100};
+    int ih[] = {100, 100, 200, 200, 300, 300, 200, 200, 100};
+    int kw[] = {10, 10, 10, 10, 10, 10, 10, 10, 20};
+    int kh[] = {10, 10, 10, 10, 10, 10, 10, 10, 20};
+    int batch_size[] = {10, 20, 10, 20, 10, 20, 10, 10, 10};
+    int in_channels[] = {10, 10, 10, 10, 10, 10, 10, 10, 10};
+    int out_channels[] = {10, 10, 10, 10, 10, 10, 10, 10, 10};
+    int stride[][2] = {{1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {5, 5}, {5, 5}};
+    int padding[][2] = {{0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {10, 10}, {10, 10}, {10, 10}};
+    int dilation[][2] = {{1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {10, 10}};
+    int padding_same[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+    std::string func_names[] = {"f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9"};
+    GeneratedFunction functions[] = {&f1, &f2, &f3, &f4, &f5, &f6, &f7, &f8, &f9};
 
-    for (int i = 0; i < n_runs; i++) {
+    for (int i = 6; i < n_runs; i++) {
         time_specialized_conv2d(iw[i], ih[i], kw[i], kh[i], batch_size[i], in_channels[i], out_channels[i], stride[i], dilation[i], padding[i], padding_same[i], functions[i], func_names[i]);
     }
 
